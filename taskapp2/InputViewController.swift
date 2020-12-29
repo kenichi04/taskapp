bcd4127f2ec85ce5a51d27f697e4c93139bddfd2
kenichi04 <slow_life_1407@yahoo.co.jp>
2020-12-30 00:00:35 +0900

    タスク登録ボタンを追加

--- taskapp2/InputViewController.swift
+++ taskapp2/InputViewController.swift
@@ -10,11 +10,12 @@ import UIKit
 import RealmSwift
 import UserNotifications
 
-class InputViewController: UIViewController {
+class InputViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var contentsTextView: UITextView!
     @IBOutlet weak var categoryTextField: UITextField!
     @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var decisionButton: UIButton!
     
     let realm = try! Realm()
     var task: Task!
@@ -29,13 +30,32 @@ class InputViewController: UIViewController {
         view.addGestureRecognizer(tapGesture)
         
         // taskインスタンスには前画面のprepareメソッド内で、新規タスクor編集するタスクを設定済み
+        titleTextField.placeholder = "入力必須です"
+        titleTextField.delegate = self
+        categoryTextField.delegate = self
+        
         titleTextField.text = task.title
         contentsTextView.text = task.contents
         categoryTextField.text = task.category
         datePicker.date = task.date // 新規タスクの場合はインスタンス生成した日時が入る
         
+        if task.title == "" {
+            decisionButton.setTitle("新規作成", for: .normal)
+        } else {
+            decisionButton.setTitle("タスク変更", for: .normal)
+        }
+        
+        if titleTextField.text == "" {
+            decisionButton.isEnabled = false
+            decisionButton.alpha = 0.3
+        } else {
+            decisionButton.isEnabled = true
+            decisionButton.alpha = 1.0
+        }
+        
     }
     
+    /*
     override func viewWillDisappear(_ animated: Bool) {
         // タスク内容を上書きする(画面遷移により非表示になるタイミング)
         try! realm.write {
@@ -51,6 +71,24 @@ class InputViewController: UIViewController {
         
         super.viewWillDisappear(animated)
     }
+    */
+    
+    @IBAction func decisionButtonAction(_ sender: Any) {
+        // タスク内容を上書きする(画面遷移により非表示になるタイミング)
+        try! realm.write {
+            task.title = titleTextField.text!
+            task.contents = contentsTextView.text
+            task.category = categoryTextField.text!
+            task.date = datePicker.date
+            realm.add(task, update: .modified)
+        }
+        // ローカル通知
+        setNotification(task: task)
+        
+        // ひとつ前のviewControllerに戻る
+        navigationController?.popViewController(animated: true)
+    }
+    
     
     // タスクのローカル通知を登録
     func setNotification(task: Task) {
@@ -96,6 +134,29 @@ class InputViewController: UIViewController {
     
     @objc func dismissKeyboard() {
         view.endEditing(true)
+        
+        if titleTextField.text == "" {
+            decisionButton.isEnabled = false
+            decisionButton.alpha = 0.3
+        } else {
+            decisionButton.isEnabled = true
+            decisionButton.alpha = 1.0
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // キーボードを閉じる
+//        textField.resignFirstResponder()
+//
+//        if titleTextField.text == "" {
+//            decisionButton.isEnabled = false
+//        } else {
+//            decisionButton.isEnabled = true
+//        }
+        
+        dismissKeyboard()
+        
+        return true
     }
     
 
