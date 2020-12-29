4c959a704d51a4c3ba09818d387b39e7aae2fd1c
kenichi04 <slow_life_1407@yahoo.co.jp>
2020-12-29 17:50:58 +0900

    ・pod install によりRealm 追加 ・.gitignore の作成

--- taskapp2/ViewController.swift
+++ taskapp2/ViewController.swift
@@ -8,13 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
+    // 表示するセル数を返す
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    // 各セルの内容を返す
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // cellインスタンス取得
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        return cell
+    }
+    
+    // 各cell選択時に実行されるdelegateメソッド
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // segue(id:cellSegue)による画面遷移
+        performSegue(withIdentifier: "cellSegue", sender: nil)
+    }
 
+    // セルの編集内容を返す
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        // 削除可能
+        return .delete
+    }
+    
+    // 編集内容の実行時に呼ばれる
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+    }
 }
 
